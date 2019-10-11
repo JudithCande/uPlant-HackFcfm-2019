f4e64feb0dbaac72a2ad5ea7cc8ddffa0bf9c98b
Osvaldo Palacios <osvaldo.delgadofl@gmail.com>
2019-10-10 20:26:11 -0500

    Empezadon con el mapa y ubicaciones

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import ReactMapGL, { Marker } from 'react-map-gl';
+import parques from './parques';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends Component {
+  state = {
+    viewport: {
+      width: "100vw",
+      height: "100vh",
+      latitude: 25.6397836,
+      longitude: -100.2931016,
+      zoom: 10
+    }
+  }
 
-export default App;
+  render(){
+    return (
+      <div>
+        {/* Pasamos nuestra informacion ...this.state.viewport
+            Cada que detecte un cambio (zoom, mover y de más) mandará
+            un arreglo con la nueva informacion viewport*/}
+        <ReactMapGL {...this.state.viewport} 
+          onViewportChange={(viewport) => this.setState({viewport})}
+          mapboxApiAccessToken="pk.eyJ1IjoidmFsZG90ZSIsImEiOiJjazFsMzZjejkwMWhiM2JuemgybzR0ZzhyIn0.-XBXaq384sEUIV7H7W3dtg">
+            {parques.map(parque => (
+              <Marker key={parque.id} latitude={parque.latitude} longitude={parque.longitude}>
+                <button className="marker-btn">
+                  <img src="/tree.svg" alt="Icono Planta"/>
+                </button>
+              </Marker>
+            ))}
+        </ReactMapGL>
+        
+      </div>
+    )
+  }
+}
